4792f91716537e5a7f9e3aff6f5e31b0c9b89e00
Markus Haverinen <mathatan@gmail.com>
2021-01-13 15:25:21 +0200

    Fix pubspec

--- example/lib/generated_plugin_registrant.dart
+++ example/lib/generated_plugin_registrant.dart
@@ -2,15 +2,12 @@
 // Generated file. Do not edit.
 //
 
-// ignore: unused_import
-import 'dart:ui';
-
 import 'package:stripe_payment/src/web/flutter_stripe_payment_web.dart';
 
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
 // ignore: public_member_api_docs
-void registerPlugins(PluginRegistry registry) {
-  StripePaymentPlugin.registerWith(registry.registrarFor(StripePaymentPlugin));
-  registry.registerMessageHandler();
+void registerPlugins(Registrar registrar) {
+  StripePaymentPlugin.registerWith(registrar);
+  registrar.registerMessageHandler();
 }
